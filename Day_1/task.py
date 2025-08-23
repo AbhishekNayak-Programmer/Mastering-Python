f045eceb3ba4fd49267dbcab228d45d6ee6bc298
Abhisheknayak-programer <sahiservice300@gmail.com>
2025-08-23 22:03:12 +0530

    Initial Commit

---
+++ Day_1/task.py
@@ -0,0 +1,8 @@
+print("Hello World")
+
+print("Abhishek Nayak" + " The Great Coder")
+
+print("The New Future of JavaScript is Web Coder Abhishek\nSo Subscribe it Now!")
+
+# The Input function helps to take the input from the user and as it is the function so that will be first executed and then after you enter the value that will be added to your print statement 
+print("Hello " + input("Enter your Name : ") + "!")
