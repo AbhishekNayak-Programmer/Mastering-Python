97b6ec8edff57f99c71c2da8a3a2b6c4bb5598ee
Abhisheknayak-programer <sahiservice300@gmail.com>
2025-08-24 17:33:01 +0530

    Day 1 Completed

--- Day_1/task.py
+++ Day_1/task.py
@@ -6,3 +6,21 @@ print("The New Future of JavaScript is Web Coder Abhishek\nSo Subscribe it Now!"
 
 # The Input function helps to take the input from the user and as it is the function so that will be first executed and then after you enter the value that will be added to your print statement 
 print("Hello " + input("Enter your Name : ") + "!")
+
+
+# Variables creating and using in python
+name = input("Enter your name : ")
+print("Hi " + name + " you name is of length " + str(len(name)) + " characters") # len function is used to find the length and str function helps to convert the length that we get in int format to string format
+
+# Only finding the length and printing
+print(len(input("Enter your Name : ")))
+
+# Naming in python programming
+user_name = "Web Coder Abhishek"
+print(user_name)
+
+# Band Name Generator Application
+print("Welcome to Brand Name Generator Application")
+city_name = input("What's your city name :\n")
+pet_name = input("What's your pet name :\n")
+print("Your band name could be : " + city_name + pet_name)
